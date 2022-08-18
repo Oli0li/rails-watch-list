be40d0835cc18544b93998146b083e8b750f2efb
Olivia Bonnal <oliviabonnal@gmail.com>
2022-08-18 12:26:16 +0200

    validations et associations OK

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, :uniqueness => {:scope => :list_id}
+end
+
+# Un signet doit être lié à un film et une liste, et les associations [film, liste] doivent être uniques.
