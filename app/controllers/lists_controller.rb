832f0c284c8780eca2d2951cbe60a316640e73ac
Olivia Bonnal <oliviabonnal@gmail.com>
2022-08-19 18:51:54 +0200

    watch list with images

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    @bookmarks = Bookmark.all
+    @bookmarks = @list.bookmarks
     @bookmark = Bookmark.new
   end
 
@@ -23,6 +23,6 @@ class ListsController < ApplicationController
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
